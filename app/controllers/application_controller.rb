de136ea3055a9c7e234f54a24ba607c53a731223
Neil D'Souza <s3600251@student.rmit.edu.au>
2017-04-28 14:03:36 +0000

    Dropdown Menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  before_filter :populate_locations, :populate_categories
+  
+  def populate_locations
+    @location_tags = Location.select(:id, :name).distinct
+  end
+
+  def populate_categories
+    @category_tags = Category.select(:id, :name).distinct
+  end
+
 end
