0297eee0b4a85f4b7a85f2cf1ad4a1cd6cc74096
Johnny <johnnyhuynhdev@gmail.com>
2017-05-07 21:02:28 +1000

    Fixed session tests, added more session tests & completed login/register system

--- test/integration/user_logout_test.rb
+++ test/integration/user_logout_test.rb
@@ -1,18 +1,31 @@
 require 'test_helper'
 
 class UserLogoutTest < ActionDispatch::IntegrationTest
+  def setup
+    # Create an existing user
+    @user = User.create(
+      name: 'Example User',
+      email: 'Example@email.com',
+      password: 'Password123',
+      password_confirmation: 'Password123'
+    )
+  end
+
   test 'logout successful' do
+    # Login
+    login_as @user
+
+    # User should be logged in first
+    assert logged_in_session?
+
     # Send destory request type to logout
     delete logout_path
 
     # Check if success message exists
     assert_not flash[:success].nil?
 
-    # Check if current user does not exist
-    assert current_user.nil?
-
     # Check if user is not logged in
-    assert_not logged_in?
+    assert_not logged_in_session?
 
     # Follow
     follow_redirect!
