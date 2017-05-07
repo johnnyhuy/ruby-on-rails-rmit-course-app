33ba6a545b42fab5edf9c0ccc57faf0565e2f46c
Neil D'Souza <s3600251@student.rmit.edu.au>
2017-05-07 15:42:04 +0000

    Create category and location

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,24 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :courses
+  
+  before_save :case_format
+  
+  validates :name,
+  presence: true,
+  length: {
+    minimum: 1,
+    maximum: 32
+  },
+  format: {
+    with: /\A[a-zA-Z\s]+\z/
+  }
+
+  # return if there is 1 record whose name equals this instances name
+  def duplicate?
+      Category.where(name: self.name).count > 0
+  end
+    
+  def case_format
+      self.name = self.name.split.map(&:capitalize).join(' ')
+  end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,18 @@
 class Location < ApplicationRecord
   has_and_belongs_to_many :courses
+  
+  validates :name,
+  presence: true,
+  length: {
+    minimum: 10,
+    maximum: 10
+  },
+  format: {
+    with: /\A\d{1,3}.\d{1,2}.{1,4}\z/
+  }
+  
+  def duplicate?
+      Location.where(name: self.name).count > 0
+  end
+  
 end
