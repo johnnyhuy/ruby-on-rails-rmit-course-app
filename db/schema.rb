665154d2896d4e6c3f0880641bda7a6c747590c5
Johnny <johnnyhuynhdev@gmail.com>
2017-05-08 01:08:21 +1000

    Refactored CSS, fixed test cases and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -68,7 +68,8 @@ ActiveRecord::Schema.define(version: 20170428024055) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "firstname"
+    t.string   "lastname"
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -4,13 +4,14 @@ class UserLoginTest < ActionDispatch::IntegrationTest
   def setup
     # User parameters
     @user_params = {
-      email: 'example@email.com',
+      email: 'example.user@rmit.edu.au',
       password: 'Password123',
     }
 
     # Create an existing user
     @user = User.create(
-      name: 'Example User',
+      firstname: 'Example',
+      lastname: 'User',
       email: @user_params[:email],
       password: @user_params[:password],
       password_confirmation: @user_params[:password]
@@ -67,25 +68,6 @@ class UserLoginTest < ActionDispatch::IntegrationTest
 
   test 'no remember token' do
     assert_not @user.authenticated?('')
-  end
-
-  test 'current user exists' do
-    # Visit login page
-    get login_url
-
-    # Login
-    post login_url,
-      params: { session: @user_params }
-
-    # Current user should exist
-    assert_not current_user.nil?
-
-    # Get the user via email
-    user = User.find_by(email: @user_params[:email])
-
-    # Found user information should be the same as current user
-    assert_equal current_user.name, user.name
-    assert_equal current_user.email, user.email
   end
 
   test 'invalid login details' do
