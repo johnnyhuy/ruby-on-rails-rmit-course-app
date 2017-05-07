2f2eee61fe9b9f5e2693fec1f4a9f07e11290373
Neil D'Souza <s3600251@student.rmit.edu.au>
2017-05-07 15:42:04 +0000

    Create category and location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-  before_filter :populate_locations, :populate_categories
+  before_action :populate_locations, :populate_categories
   
   def populate_locations
     @location_tags = Location.select(:id, :name).distinct
