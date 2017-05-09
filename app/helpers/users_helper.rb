cc7394b4efcbafd51f5cd3aa3e05ad62711dc967
Johnny <johnnyhuynhdev@gmail.com>
2017-05-09 13:39:42 +1000

    Redesigned frontend parts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,11 @@
 module UsersHelper
-  # Returns the Gravatar for the given user.
-  def gravatar_for(user)
+  def gravatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: "Gravatar", class: "gravatar")
+  end
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    image_tag(gravatar_url(user), alt: "Gravatar", class: "gravatar")
   end
 end
